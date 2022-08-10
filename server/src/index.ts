67653cfcbbcba916cdd036685c54a1ce7db30833
Giga <g.uchaneishvili@gmail.com>
2022-08-10 14:46:52 +0400

    Swagger is added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose, { ConnectOptions } from "mongoose";
 import config from "./config";
 import app from "./app";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 mongoose
   .connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`, {
@@ -13,6 +15,21 @@ mongoose
   .catch((err) => {
     console.log(`Initial Insta API Database connection error occured -`, err);
   });
+
+const swaggerOption = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Insta Api ",
+      version: "1.0.0",
+    },
+  },
+  // ['.routes/*js]
+  apis: ["index.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOption);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(3001, () => {
   console.log("started");
